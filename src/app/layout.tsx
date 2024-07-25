867d9a799ecc739ad40154bd461baf8629f9b7b1
Johny-Lannister <juanfragale11@gmail.com>
2024-07-25 13:28:12 -0300

    dadaw

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,12 +12,12 @@ export const metadata: Metadata = {
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode
-}) {
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="es">
+      <body className={montserrat.className}>{children}</body>
     </html>
   )
 }
