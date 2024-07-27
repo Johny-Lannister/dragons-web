2f89c602fc08715bbd6a09426b11fbd0ef2d1c57
Johny-Lannister <juanfragale11@gmail.com>
2024-07-26 22:57:38 -0300

    32

--- src/app/components/Dragons.tsx
+++ src/app/components/Dragons.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@nextui-org/react";
 import React, { useState, useEffect } from "react";
+
 
 interface Dragon {
   id: number;
@@ -9,35 +11,127 @@ interface Dragon {
   age: string;
   color: string;
   riders: string;
+  show: string;
 }
+
 
 const Dragons: React.FC = () => {
   const [dragonsData, setDragonsData] = useState<Dragon[]>([]);
+  const [dragonDataFilter, setDragonDataFilter] = useState<string>('All dragons');
+  const [isOpen, setIsOpen] = useState(false);
+
+    function getMenuClasses() {
+        let menuClasses = [
+            'md:flex',
+            'md:items-center',
+            'md:gap-6',
+            'z-20'
+        ];
+        if (isOpen) {
+            menuClasses.push(
+                'flex',
+                'absolute',
+                'top-[60px]',
+                'bg-emerald-50',
+                'w-full',
+                'left-0',
+                'p-4',
+                'gap-10',
+                'flex-col',
+            );
+        } else {
+            menuClasses.push('hidden');
+        }
+        return menuClasses.join(' ');
+    }
   useEffect(() => {
-    fetch("https://dragons-api.vercel.app/api/dragons")
+    fetch("https://dragons-api.vercel.app")
       .then((response) => response.json())
       .then((data) => setDragonsData(data))
       .catch((error) => console.error("Error fetching dragons data:", error));
   }, []);
 
+  const filterChange = (filter: string) => {
+    setDragonDataFilter(filter)
+  }
+
+  const filteredDragons = dragonsData.filter(dragon => {
+    if (dragonDataFilter === 'All Dragons') return true;
+    if (dragonDataFilter === 'Game of Thrones') return dragon.show === 'GOT';
+    if (dragonDataFilter === 'House of The Dragon') return dragon.show === 'HOTD';
+    return true;
+  });
+
   return (
-    <div className="mx-12 pb-40">
-      {/* Aquí puedes renderizar tus datos */}
-        <div className="grid grid-cols-2 md:grid-cols-3 gap-10">
-      {dragonsData.map((dragon) => (
-            <div key={dragon.id}>
-                <img className="h-72 w-full max-w-full rounded-lg" src={dragon.image} alt=""/>
-                    <div className="space-y-2">
-                        <h1 className="text-2xl font-bold mt-4">{dragon.name}</h1>
-                        <p> <strong>Color: </strong> {dragon.color}</p>
-                        <p> <strong>Age: </strong>{dragon.age}</p>
-                        <p> <strong>Size: </strong>{dragon.size}</p>
-                        <p> <strong>Description: </strong> {dragon.description}</p>
-                        <p> <strong>Rider/s: </strong>{dragon.riders}</p>
-                    </div>
+    <div>
+      <nav className=" py-10 my-12 bg-slate-100 text-gray-1000 p-4 sm:p-6 md:flex md:justify-between md:items-center">
+            <div className="container mx-auto flex items-center justify-between">
+                <div className={getMenuClasses()}>
+                <button className="mx-2 hover:text-gray-300" onClick={() => filterChange('All Dragons')}>
+              All Dragons
+            </button>
+            <button className="mx-2 hover:text-gray-300" onClick={() => filterChange('Game of Thrones')}>
+              Game of Thrones
+            </button>
+            <button className="mx-2 hover:text-gray-300" onClick={() => filterChange('House of The Dragon')}>
+              House of The Dragon
+            </button>
+                </div>
+                <div className="md:hidden flex items-center">
+                    <button onClick={() => setIsOpen(!isOpen)}>
+                        {isOpen ? (
+                            <svg
+                                className="w-6 h-6"
+                                fill="none"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="M6 18L18 6M6 6l12 12"
+                                ></path>
+                            </svg>
+                        ) : (
+                            <svg
+                                className="w-6 h-6"
+                                fill="none"
+                                stroke="currentColor"
+                                viewBox="0 0 24 24"
+                                xmlns="http://www.w3.org/2000/svg"
+                            >
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth={2}
+                                    d="M4 6h16M4 12h16m-7 6h7"
+                                ></path>
+                            </svg>
+                        )}
+                    </button>
+                </div>
             </div>
-      ))}
-        </div>
+        </nav>
+      <div className="mx-12 pb-40">
+          <div className="grid grid-cols-2 md:grid-cols-3 gap-10">
+        {filteredDragons.map((dragon) => (
+              <div key={dragon.id}>
+                  <img className="h-72 w-full max-w-full rounded-lg" src={dragon.image} alt=""/>
+                      <div className="space-y-2">
+                          <h1 className="text-2xl font-bold mt-4">{dragon.name}</h1>
+                          <p> <strong>Color: </strong> {dragon.color}</p>
+                          <p> <strong>Age: </strong>{dragon.age}</p>
+                          <p> <strong>Size: </strong>{dragon.size}</p>
+                          <p> <strong>Description: </strong> {dragon.description}</p>
+                          <p> <strong>Rider/s: </strong>{dragon.riders}</p>
+                          <p> <strong>Show: </strong>{dragon.show}</p>
+                      </div>
+              </div>
+        ))}
+          </div>
+      </div>
     </div>
   );
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,21 +7,22 @@ import Dragons from './components/Dragons';
 
 const HomePage = () => {
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen bg-slate-50 text-gray-800">
-        <img
+    <main className="my-8 flex flex-col items-center justify-center min-h-screen bg-slate-50 text-gray-800">
+        <img 
           src="/logo.jpg"
           alt="Logo"
-          className="relative z-10 w-24 h-24 rounded-full mb-4 md:w-60 md:h-60"
+          className="relative z-10 w-24 h-24 rounded-full  mb-2 md:w-60 md:h-60"
         />
-        <h1 className="text-4xl py-4 center md:text-6xl lg:text-7xl xl:text-6xl font-bold mb-4 relative z-10 align: center">Song of Ice And Fire Dragons</h1>
+        <h1 className="text-4xl py-4 center md:text-6xl lg:text-7xl xl:text-6xl font-bold mb-2 relative z-10 align: center">Song of Ice And Fire Dragons</h1>
 
-      <section className="w-full py-21 px-4 md:px-8 bg-slate-50" id='tools'>
+      <section className="w-full py-21 px-4 md:px-8 bg-slate-50" id='all-dragons'>
         <div className="max-w-6xl mx-auto">
-          <p className="text-lg md:text-xl text-gray-800 mb-12">
+          <p className="text-lg md:text-xl text-gray-800 my-8 mb-2">
           This is the complete list with all the dragons known in the Song of Ice and Fire books and the TV shows, Game of Thrones and House of The Dragon.
           </p>
         </div>
       </section>
+      {/* <Navbar /> */}
     <Dragons />
     </main>
   );
